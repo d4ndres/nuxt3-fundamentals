3ae1cc75da629603f080c0597ca5bec639c65153
d4ndres <d4ndres@gmail.com>
2023-02-22 23:06:45 -0600

    dyanamic route params

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/movies">Movies</NuxtLink>
+      <NuxtLink to="https://vueschool.io">Vue school</NuxtLink>
+    </nav>
     <NuxtPage />
   </div>
 </template>
+<style scoped>
+  nav {
+    display: flex;
+    gap: 1rem;
+  }
+  a {
+    color: hotpink;
+    text-decoration: none;
+    transition: .4s;
+    
+  }
+  a:hover {
+    color: cyan
+  }
+</style>
---
+++ pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    I'm on team <strong>{{ $route.params.teamSlug}}</strong>
+
+    and my user id is {{ $route.params.userId }}
+  </div>
+</template>
+
+<style scoped></style>
